9c9280a9b9d426a0a8b6477bfaa230cd65f9d27b
Arnold Zokas <arnold.zokas@coderoom.net>
2010-01-08 19:57:17 +0000

    created specification project Artisan.Specifications

---
+++ source/Artisan.Specifications/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿//  #######################################################
+//  
+//  # Copyright © Arnold Zokas 2010
+//  
+//  # This source code is subject to terms and conditions of the Microsoft Public License.
+//  # A copy of the license can be found in the license.txt file at the root of this distribution.
+//  # If you cannot locate the Microsoft Public License, please send an email to arnold.zokas@coderoom.net.
+//  # By using this source code in any fashion, you are agreeing to be bound by the terms of the Microsoft Public License.
+//  # You must not remove this notice, or any other, from this software.
+//  
+//  #######################################################
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Artisan.Specifications")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Artisan.Specifications")]
+[assembly: AssemblyCopyright("Copyright © Arnold Zokas 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("3a1924a1-196f-41bc-a575-ad7562bf55bd")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
